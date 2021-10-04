7d470bc976d5950bf1220cef9eb49f0f3402baff
sharifsikder <sharif01742088325@gmail.com>
2021-10-04 06:28:59 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Services from './Components/Services/Services'
+import NotFound from './Components/NotFound/NotFound';
+import Navber from './Components/Navbar/Navber';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+   <Router>
+     <Navber></Navber>
+     <Switch>
+      <Route exact path="/">
+      <Home></Home>
+      </Route>
+      <Route path="/home">
+        <Home></Home>
+      </Route>
+      <Route path="/about">
+        <About></About>
+      </Route>
+      <Route path="/contact">
+        <Contact></Contact>
+      </Route>
+      <Route path="/services">
+        <Services></Services>
+      </Route>
+      <Route exact path="/*">
+        <NotFound></NotFound>
+      </Route>
+     </Switch>
+     <Footer></Footer>
+   </Router>
+
     </div>
   );
 }
---
+++ src/Components/Navbar/Navber.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import  './Navbar.css'
+
+const Navber = () => {
+    return (
+        <div>
+            
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container
+  >
+  <Navbar.Brand href="#home" className="logo-name"> Courses Online</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+
+    
+              <ul className="menu-bar ">
+                <Link to="/home" className="items">
+                  <li>Home</li>
+                </Link>
+                <Link to="/services" className="items">
+                  <li>Services</li>
+                </Link>
+                <Link to="/about" className="items">
+                  <li>About</li>
+                </Link>
+                <Link to="contact" className="items">
+                  <li>Contact us</li>
+                </Link>
+              </ul>
+            
+
+
+     
+    </Nav>
+   
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+        </div>
+    );
+};
+
+export default Navber;
